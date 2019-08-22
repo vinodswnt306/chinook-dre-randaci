5776de12e3fe7ec6d4989ecc756cbdbe87b7b1da
Vinod Sawant <50289281+vinodswnt306@users.noreply.github.com>
2019-08-22 19:48:07 +0530

    Update top_agent.sql

--- sql/top_agent.sql
+++ sql/top_agent.sql
@@ -6,3 +6,9 @@ join customer c on c.customerid =  i.customerid
 join employee e on e.employeeid = c.supportrepid
 group by e.Employeeid
 )
+
+#OR WE CAN USE
+
+SELECT E.FIRSTNAME,E.LASTNAME,SUM(TOTAL) FROM EMPLOYEE E INNER JOIN CUSTOMER C ON C.SUPPORTREPID = E.EMPLOYEEID
+									INNER JOIN INVOICE USING(CUSTOMERID) GROUP BY E.EMPLOYEEID  ORDER BY
+                                    SUM(TOTAL) DESC LIMIT 1;
